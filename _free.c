3f3a25ca192caaddf09c74fc90a4b603d3b3f60d
Makanaka-Mitchelle <chawiramitchelle@gmail.com>
2023-11-10 10:31:49 -0800

    free_all

--- _free.c
+++ _free.c
@@ -1,5 +1,13 @@
 #include "shell.h"
 
+/**
+ * hsh_freeall - function that free up the memory
+ * @text: the allocated space to store the text
+ * @token: the array of strings
+ * @path: the path
+ * @fullpath: the fullpath
+ */
+
 void hsh_freeall(char *text, char **token, char *path, char *fullpath)
 {
 	free(text);

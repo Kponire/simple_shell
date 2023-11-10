ca415921e62273d2604a8a059c555a279d3a424c
Makanaka-Mitchelle <chawiramitchelle@gmail.com>
2023-11-10 10:22:12 -0800

    execute

--- _execute.c
+++ _execute.c
@@ -1,5 +1,12 @@
 #include "shell.h"
 
+/**
+ * _execute - function that execute the execve which takes env, path, command
+ * @fullpath: the fullpath
+ * @command: the input string
+ * Return: 0 which indicates success
+ */
+
 int _execute(char *fullpath, char **command)
 {
 	pid_t proc;

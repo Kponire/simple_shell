b0a009b76ebc2fc5c2c5d662ed8d80bb97335c85
Makanaka-Mitchelle <chawiramitchelle@gmail.com>
2023-11-10 10:12:59 -0800

    _echo

--- _echo.c
+++ _echo.c
@@ -1,10 +1,16 @@
 #include "shell.h"
 
+/**
+ * _echo - function that implements the echo builtin command in unix
+ * @token: the inpit string
+ * Return: 0 which indicates success otherwise 1
+ */
+
 int _echo(char **token)
 {
 	char *path;
 	pid_t ppid;
-	
+
 	ppid = getppid();
 	if (strcmp(token[1], "$?") == 0)
 	{

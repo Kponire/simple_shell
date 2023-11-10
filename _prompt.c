3c3142284bb24ba79ff983c7815eefa22beb395e
Makanaka-Mitchelle <chawiramitchelle@gmail.com>
2023-11-10 10:40:53 -0800

    prompt

--- _prompt.c
+++ _prompt.c
@@ -1,5 +1,11 @@
 #include "shell.h"
 
+/**
+ * _prompt - the prints $ to the stdout
+ * @fd: the file descriptor
+ * @buf: the struct stat
+ */
+
 void _prompt(int fd, struct stat buf)
 {
 	fstat(fd, &buf);
